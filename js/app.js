d993f17e0718756899939479c6b82d88d0df8403
AliBakerSartawi <alisartawi@live.com>
2021-03-24 08:13:43 +0200

    fixed sorting except default

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 Unicorn.all = [];
 let unicornPageOne = [];
 let unicornPageTwo = [];
+let pageNumber = 0;
 
 //////// Constructor
 function Unicorn(unicorn) {
@@ -23,10 +24,10 @@ Unicorn.prototype.renderImage = function() {
 };
 
 //////// render filter
-Unicorn.prototype.renderFilter = function(filterPageArr) {
+Unicorn.prototype.renderFilter = function(pageArr) {
   let preventRepeat = 1;
 
-  filterPageArr.forEach(element => {
+  pageArr.forEach(element => {
     if (element.keyword === this.keyword){
       preventRepeat --;
       // console.log(preventRepeat);
@@ -46,7 +47,7 @@ const ajaxSettings = {
   dataType: 'json',
 };
 
-function ajaxAndRender(url, pageNum, filterPageArr, renderOrNot) {
+function ajaxAndRender(url, pageNum, pageArr, renderOrNot) {
   $.ajax(url, ajaxSettings).then(data => {
     data.forEach(unicorn => {
       let unicornObject = new Unicorn(unicorn);
@@ -63,105 +64,121 @@ function ajaxAndRender(url, pageNum, filterPageArr, renderOrNot) {
       //populate window and filter with images
       if (renderOrNot === true){
         unicornObject.renderImage();
-        unicornObject.renderFilter(filterPageArr);
+        unicornObject.renderFilter(pageArr);
       }
     });
   });
+  pageNumber = 1;
 }
 //////// initial rendering on load, and instantiating objects for both pages
 ajaxAndRender('data/page-1.json', 1, unicornPageOne, true);
 ajaxAndRender('data/page-2.json', 2, unicornPageTwo, false);
+defaultButtonCheck();
 
 //////// filter selection event
 $('select').change(function() {
-  // console.log(this.value);
+  // console.log(Unicorn.all);
   Unicorn.all.forEach(element => {
     if ('default' === this.value){
       $(`.${element.keyword}`).show();
+      // console.log(element.title);
     } else if (element.keyword !== this.value) {
       $(`.${element.keyword}`).hide();
+      // console.log(element.title);
     } else {
       $(`.${element.keyword}`).show();
+      // console.log(element.title);
     }
   });
 });
 
-
 //////// page selection event
-function pageChange(pageNum, filterPageArr) {
+function pageOrSortChange(pageArr, pageNum) {
   ////remove images and filters
   $('.animal').remove();
   $('.filter').remove();
-  // Unicorn.all.forEach(element => {
-  //   console.log(element.page);
-  //   if (element.page === pageNum){
-  //     console.log(element.page);
-  //     element.renderImage();
-  //     element.renderFilter(filterPageArr);
-  //   }
-  // });
-  filterPageArr.forEach(element => {
+
+  pageArr.forEach(element => {
     element.renderImage();
-    element.renderFilter(filterPageArr);
+    element.renderFilter(pageArr);
   });
+  pageNumber = pageNum;
 }
 $('#page-one').click(function() {
-  pageChange(1, unicornPageOne);
+  pageOrSortChange(unicornPageOne, 1);
 });
 $('#page-two').click(function() {
-  pageChange(2, unicornPageTwo);
+  pageOrSortChange(unicornPageTwo, 2);
 });
 
 
-
-///////title sorting function
-console.log(unicornPageOne);
-
-
-unicornPageOne.sort((a, b) => {
-  let fa = a.title.toLowerCase();
-  let fb = b.title.toLowerCase();
-  console.log(unicornPageOne);
-
-  if (fa < fb) {
-    return -1;
+/////// sorting radio buttons
+function defaultButtonCheck() {
+  $('#by-title').prop('checked', true);
+  if ($('#by-title').is(':checked')) {
+    if (pageNumber === 1){
+      console.log('check');
+      sortArrByTitle(unicornPageOne);
+      pageOrSortChange(unicornPageOne);
+    } else {
+      sortArrByTitle(unicornPageTwo);
+      pageOrSortChange(unicornPageTwo);
+    }
   }
-  if (fa > fb) {
-    return 1;
-  }
-  return 0;
-});
-
-/////// sorting test, works fine
-let testArr = [
-  {number: 3},
-  {number: 6},
-  {number: 17},
-];
-// console.log(testArr);
-testArr.sort((a, b) => {
-  let fa = a.horns;
-  let fb = b.horns;
-  // console.log(testArr);
-
-  if (fa < fb) {
-    return -1;
-  }
-  if (fa > fb) {
-    return 1;
-  }
-  return 0;
-});
-
-
-///////// sorting radio buttons
-// if ($('#by-title').is(':checked')) {
-//   alert('wow');
-// }
+}
 
 $('#by-title').click(function() {
   if ($(this).is(':checked')) {
-    alert('wow');
+    if (pageNumber === 1){
+      sortArrByTitle(unicornPageOne);
+      pageOrSortChange(unicornPageOne);
+    } else {
+      sortArrByTitle(unicornPageTwo);
+      pageOrSortChange(unicornPageTwo);
+    }
   }
 });
 
+function sortArrByTitle(pageArr){
+  pageArr.sort((a, b) => {
+    let fa = a.title.toLowerCase();
+    let fb = b.title.toLowerCase();
+    console.log(pageArr);
+
+    if (fa < fb) {
+      return -1;
+    }
+    if (fa > fb) {
+      return 1;
+    }
+    return 0;
+  });
+}
+
+$('#by-horn').click(function() {
+  if ($(this).is(':checked')) {
+    if (pageNumber === 1){
+      sortArrByHorn(unicornPageOne);
+      pageOrSortChange(unicornPageOne);
+    } else {
+      sortArrByHorn(unicornPageTwo);
+      pageOrSortChange(unicornPageTwo);
+    }
+  }
+});
+
+function sortArrByHorn(pageArr){
+  pageArr.sort((a, b) => {
+    let fa = a.horns;
+    let fb = b.horns;
+    console.log(pageArr);
+
+    if (fa < fb) {
+      return -1;
+    }
+    if (fa > fb) {
+      return 1;
+    }
+    return 0;
+  });
+}
